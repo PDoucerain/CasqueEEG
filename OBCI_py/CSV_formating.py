b254ae0a42c36d7cf02a18b20b4688409008dd3c
William Babin-Demers <babw2501@usherbrooke.ca>
2020-01-30 16:51:45 -0500

    Ajout d'un filtre BP butterworth et nettoyage du code

--- OBCI_py/CSV_formating.py
+++ OBCI_py/CSV_formating.py
@@ -3,9 +3,9 @@ import pandas as pd
 import scipy as sc
 from scipy.io import loadmat
 from scipy import signal
+import matplotlib.pyplot as pyplot
 import sklearn
 from sklearn import linear_model
-import matplotlib.pyplot as pyplot
 from matplotlib import style
 import pickle
 import csv
@@ -14,7 +14,10 @@ labels = ['AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', '
 OV_labels = ['Time:128Hz', 'Epoch', 'AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4', 'Status', 'Ref_Nose', 'Event Id', 'Event Date', 'Event Duration']
 
 # ---Importation des donnees du .mat--- #
-data = loadmat("SSVEP_Training/Dataset/U001ai.mat")
+data_filename = 'U001ai.mat'
+data_folder = 'SSVEP_Training/Dataset/'
+data_path = data_folder + data_filename
+data = loadmat(data_path)
 datacsv = data['eeg']
 events = data['events']
 
@@ -25,11 +28,12 @@ timestep = 1/freq_ech
 samples = freq_ech*exp_time
 ep_lgt = exp_time*freq_ech
 ref_nose = 4179.179
+PI = 3.1416
 
 # ---Frequences utilisees par SSVEP--- #
 f_SSVEP = [12.00, 10.00, 8.57, 7.50, 6.66]
-#wn = (1/(freq_ech/2)) * f_SSVEP    #Freq normalise 0 a nyquist
-
+bp_freq = [f_SSVEP[-1], f_SSVEP[0]]
+bp_wn = [i * 2.0/freq_ech for i in bp_freq]         # for butterworth filter
 
 datacsv = list(datacsv)
 datacsv = np.transpose(datacsv)
@@ -38,25 +42,20 @@ nb_channels = len(datacsv[0])
 nb_epoch = len(datacsv)//ep_lgt
 print("nb_channels :", nb_channels)
 print("nb_epoch :", nb_epoch)
-#Ajout des labels des données
-df = pd.DataFrame(data=datacsv, columns=labels)
 
+# ---Ajout des labels des données--- #
+df = pd.DataFrame(data=datacsv, columns=labels)
 timestamp = np.linspace(0, len(datacsv)/freq_ech, len(datacsv))
 
-# Epoch definition
-epoch = []
+# ---Epoch definition--- #
+epoch_label = []
 j = 0
 for ii in range(int(len(datacsv)/(5*freq_ech))+1):
-    for jj in range(5*freq_ech):
-        epoch.append(j)
-        if len(epoch) == len(datacsv):
+    for jj in range(exp_time*freq_ech):
+        epoch_label.append(j)
+        if len(epoch_label) == len(datacsv):
             break
     j += 1
-
-# -----FFT----- #
-#Test avec sinus
-#x = np.linspace(0.0, samples*exp_time, samples)
-#y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
 
 fft_data = np.array(datacsv)
 print("fftdata :", len(datacsv))
@@ -65,7 +64,6 @@ fft_data = np.subtract(fft_data, ref_nose)
 
 # ---Definition d'une epoch--- #
 epoch = nb_epoch * [fft_data[0:ep_lgt, 1]]
-print(np.shape(epoch))
 
 # ---Definition d'un channel--- #
 channel = nb_channels*[None]
@@ -75,40 +73,39 @@ for i in range(nb_channels):
 print("longueur channel :", np.shape(channel))
 
 # ---Integration des donnees dans array 3D [channel][epoch][echantillon]--- #
-for chan in range(nb_channels):
-    for epo in range(1, nb_epoch):
-        channel[chan][epo] = fft_data[((ep_lgt*epo)-ep_lgt):(ep_lgt*epo), chan]
-        #print("channel : {} | epoc : {} | value : {}".format(chan, ep, channel[chan][ep][0]))
-    print("ep : {}, chan : {},".format(epo, chan), channel[10][8][0])
+for c in range(nb_channels):
+    for e in range(1, nb_epoch):
+        channel[c][e] = fft_data[((ep_lgt*e)-ep_lgt):(ep_lgt*e), c]
 
-#print(fft_data[((ep_lgt*ep)-ep_lgt):(ep_lgt*ep), chan])
-#print("echantillon 46, 3e epoch, 1 channel", channel[0][3])
+# ---Choix du channel et de l'epoch--- #
+chan = 6
+epo = 0
 
-#fft_data = datacsv[8070:8710, 6]
-#for i in range(0, len(fft_data)):
-#    fft_data[i] = [x-ref_nose for x in fft_data[i][:, ]]
-#print(channel[10][10])
-fft_channel_1 = np.fft.fft(channel[3][2])
+# ---Filtrage passe-bande des donnees--- #
+[b, a] = sc.signal.butter(5, bp_wn, btype='bandpass')
+fft_channel_1 = signal.filtfilt(b, a, channel[chan][epo])
+#fft_channel_1 = channel[5][3]
+fft_channel_1 = np.fft.fft(fft_channel_1)
+
 #print(channel[0][8])
 fig, ax = pyplot.subplots()
 xf = np.linspace(0.0, freq_ech/2, int(samples/2))
 ax.plot(xf, 2/samples * abs(fft_channel_1[:samples//2]))
+ax.set_ylabel('uvolts')
+ax.set_xlabel('Hz')
+ax.set_title('Channel eeg {}, epoch : {}'.format(labels[chan], epo+1))
 #ax.plot(np.linspace(0, len(fft_data), freq_ech*5), fft_data)
 pyplot.show()
 
+# ---Detection de la frequence predominante--- #
 fft_channels = 2/samples * abs(fft_channel_1[:samples//2])
 #max_freq = [i for i, x in enumerate(fft_channels[5:]) if x == max(fft_channels[5:])]
 max_freq = np.argsort(fft_channels)
 max_freq = max_freq[::-1]
 
-# ---Filtrage passe-bande des donnees--- #
-sc.signal.butter(5, btype='bandpass')
-
-#for f in fft_FC5
-
 # Ajout des colonnes manquantes au CSV
 df['Time:128Hz'] = timestamp
-df['Epoch'] = epoch
+df['Epoch'] = epoch_label
 df['Event Id'] = 0
 df['Event Date'] = 0
 df['Event Duration'] = 0
@@ -118,4 +115,4 @@ df['Ref_Nose'] = 4179.179
 df = df[OV_labels]
 
 # Conversion de la liste vers CSV
-df.to_csv('U001ai.csv', index=False, header=True)
+df.to_csv(data_filename, index=False, header=True)
